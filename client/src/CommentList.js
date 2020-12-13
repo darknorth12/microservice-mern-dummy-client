f5d16e81dc55a925d22e0871b65b8fa85d053b23
Ravi Ranwa <ravi.ranwa@outlook.com>
2020-12-13 14:16:58 +0530

    status : pending, accpeted , rejected changes

--- client/src/CommentList.js
+++ client/src/CommentList.js
@@ -1,22 +1,21 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
-const CommentList = ({ postId }) => {
-  const [comments, setComments] = useState([]);
-
-  const fetchComments = async () => {
-    const res = await axios.get(
-      `http://localhost:4001/posts/${postId}/comments`
-    );
-    setComments(res.data);
-  };
-
-  useEffect(() => {
-    fetchComments();
-  }, []);
-
+const CommentList = ({ comments }) => {
   const renderComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content;
+
+    switch (comment.status) {
+      case "accepted":
+        content = comment.content;
+        break;
+      case "pending":
+        content = "This comment is pending in moderation";
+        break;
+      case "rejected":
+        content = "This comment has been rejected";
+        break;
+    }
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{renderComments}</ul>;
